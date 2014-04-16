fe0e92e7ca7b837d601def9154de20768ec68b5f
Anthony Lewis <tony.lewis@schmoozecom.com>
2014-04-16 16:36:39 -0500

    new license tags

--- functions.inc.php
+++ functions.inc.php
@@ -1,4 +1,7 @@
 <?php
+//	License for all code of this FreePBX module can be found in the license file inside the module directory
+//	Copyright 2013 Schmooze Com Inc.
+//
 function setup_userman() {
 	if(version_compare(getVersion(), '12.0', '<')) {
 		if(!interface_exists('BMO')) {
