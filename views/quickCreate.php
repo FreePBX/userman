ffd5cee576f21a1c40b1011c56505c9b3f3c36e6
Andrew Nagy <andrew.nagy@the159.com>
2015-03-05 22:00:09 -0500

    FREEPBX-8446 userman quick create work

---
+++ views/quickCreate.php
@@ -0,0 +1,10 @@
+<div class="form-group">
+	<label for="um"><?php echo _('Enable Usermanager')?> <i class="fa fa-question-circle fpbx-help-icon" data-for="um"></i></label><br/>
+	<span class="radioset">
+		<input type="radio" name="um" id="um_on" value="yes">
+		<label for="um_on"><?php echo _('Yes')?></label>
+		<input type="radio" name="um" id="um_off" value="no" checked>
+		<label for="um_off"><?php echo _('No')?></label>
+	</span>
+	<span id="um-help" class="help-block fpbx-help-block"><?php echo _('Enable User Manager for this extension')?></span>
+</div>
