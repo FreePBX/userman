a8cb2bd19a863b58f5344665833fa918fc3558fb
Andrew Nagy <andrew.nagy@the159.com>
2015-06-09 15:23:08 -0400

    Better quick create visuals

--- views/quickCreate.php
+++ views/quickCreate.php
@@ -1,10 +1,27 @@
-<div class="form-group">
-	<label for="um"><?php echo _('Enable Usermanager')?> <i class="fa fa-question-circle fpbx-help-icon" data-for="um"></i></label><br/>
-	<span class="radioset">
-		<input type="radio" name="um" id="um_on" value="yes">
-		<label for="um_on"><?php echo _('Yes')?></label>
-		<input type="radio" name="um" id="um_off" value="no" checked>
-		<label for="um_off"><?php echo _('No')?></label>
-	</span>
-	<span id="um-help" class="help-block fpbx-help-block"><?php echo _('Enable User Manager for this extension')?></span>
+<div class="element-container">
+	<div class="row">
+		<div class="col-md-12">
+			<div class="row">
+				<div class="form-group">
+					<div class="col-md-3">
+						<label class="control-label" for="um"><?php echo _('Enable Usermanager')?></label>
+						<i class="fa fa-question-circle fpbx-help-icon" data-for="um"></i>
+					</div>
+					<div class="col-md-9">
+						<span class="radioset">
+							<input type="radio" name="um" id="um_on" value="yes">
+							<label for="um_on"><?php echo _('Yes')?></label>
+							<input type="radio" name="um" id="um_off" value="no" checked>
+							<label for="um_off"><?php echo _('No')?></label>
+						</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+			<span id="um-help" class="help-block fpbx-help-block"><?php echo _('Enable User Manager for this extension')?></span>
+		</div>
+	</div>
 </div>
