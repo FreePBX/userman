1bc13177ab83f233552c69f9bae9398ca8a014f2
Andrew Nagy <andrew.nagy@the159.com>
2015-07-08 14:15:56 -0400

    [Module Tag script: userman 13.0.5]

---
+++ functions.inc/auth/modules/Msad.php
@@ -0,0 +1,496 @@
+<?php
+// vim: set ai ts=4 sw=4 ft=php:
+//	License for all code of this FreePBX module can be found in the license file inside the module directory
+//	Copyright 2013 Schmooze Com Inc.
+//
+namespace FreePBX\modules\Userman\Auth;
+
+class Msad extends Auth {
+	/**
+	 * LDAP Object
+	 * @var object
+	 */
+	private $ldap = null;
+	/**
+	 * LDAP Host
+	 * @var string
+	 */
+	private $host = '';
+	/**
+	 * LDAP Port
+	 * @var integer
+	 */
+	private $port = 389;
+	/**
+	 * LDAP Base DN
+	 * @var string
+	 */
+	private $dn = "";
+	/**
+	 * LDAP Domain
+	 * @var string
+	 */
+	private $domain = "";
+	/**
+	 * LDAP User
+	 * @var string
+	 */
+	private $user = "";
+	/**
+	 * LDAP Password
+	 * @var string
+	 */
+	private $password = "";
+	/**
+	 * User cache
+	 * cache requests throughout this class
+	 * @var array
+	 */
+	private $ucache = array();
+	/**
+	 * Group Cache
+	 * cache requests throughout this class
+	 * @var array
+	 */
+	private $gcache = array();
+
+	public function __construct($userman, $freepbx) {
+		parent::__construct($userman, $freepbx);
+		$config = $userman->getConfig("authMSADSettings");
+		$this->host = $config['host'];
+		$this->port = !empty($config['port']) ? $config['port'] : 389;
+		$this->dn = $config['dn'];
+		$this->domain = $config['domain'];
+		$this->user = $config['username'];
+		$this->password = $config['password'];
+	}
+
+	/**
+	* Get information about this authentication driver
+	* @param  object $userman The userman object
+	* @param  object $freepbx The FreePBX BMO object
+	* @return array          Array of information about this driver
+	*/
+	public static function getInfo($userman, $freepbx) {
+		if(!function_exists('ldap_connect')) {
+			return array();
+		}
+		return array(
+			"name" => _("Microsoft Active Directory")
+		);
+	}
+
+	/**
+	 * Get the configuration display of the authentication driver
+	 * @param  object $userman The userman object
+	 * @param  object $freepbx The FreePBX BMO object
+	 * @return string          html display data
+	 */
+	public static function getConfig($userman, $freepbx) {
+		$config = $userman->getConfig("authMSADSettings");
+		$status = array(
+			"connected" => false,
+			"type" => "info",
+			"message" => _("Not Connected")
+		);
+		if(!empty($config['host']) && !empty($config['username']) && !empty($config['password']) && !empty($config['domain'])) {
+			$msad = new static($userman, $freepbx);
+			try {
+				$msad->connect();
+				$status = array(
+					"connected" => true,
+					"type" => "success",
+					"message" => _("Connected")
+				);
+			} catch(\Exception $e) {
+				$status = array(
+					"connected" => false,
+					"type" => "danger",
+					"message" => $e->getMessage()
+				);
+			}
+		} elseif(!empty($config['host']) || !empty($config['username']) || !empty($config['password']) || !empty($config['domain'])) {
+			$status = array(
+				"connected" => false,
+				"type" => "warning",
+				"message" => _("Not all of the connection parameters have been filled out")
+			);
+		}
+		return load_view(dirname(dirname(dirname(__DIR__)))."/views/msad.php", array("config" => $config, "status" => $status));
+	}
+
+	/**
+	 * Save the configuration about the authentication driver
+	 * @param  object $userman The userman object
+	 * @param  object $freepbx The FreePBX BMO object
+	 * @return mixed          Return true if valid. Otherwise return error string
+	 */
+	public static function saveConfig($userman, $freepbx) {
+		$config = array(
+			"host" => $_REQUEST['msad-host'],
+			"port" => $_REQUEST['msad-port'],
+			"username" => $_REQUEST['msad-username'],
+			"password" => $_REQUEST['msad-password'],
+			"domain" => $_REQUEST['msad-domain'],
+			"dn" => $_REQUEST['msad-dn']
+		);
+		if(!empty($config['host']) && !empty($config['username']) && !empty($config['password']) && !empty($config['domain'])) {
+			$msad = new static($userman, $freepbx);
+			try {
+				$msad->connect();
+				$msad->sync();
+			} catch(\Exception $e) {}
+		}
+		$userman->setConfig("authMSADSettings", $config);
+		return true;
+	}
+
+	/**
+	 * Return the LDAP object after connect
+	 * @return object The LDAP object
+	 */
+	public function getLDAPObject() {
+		$msad->connect();
+		return $this->ldap;
+	}
+
+	/**
+	 * Connect to the LDAP server
+	 */
+	public function connect() {
+		if(!$this->ldap) {
+			$this->ldap = ldap_connect($this->host,$this->port);
+			if($this->ldap === false) {
+				$this->ldap = null;
+				throw new \Exception("Unable to Connect");
+			}
+			ldap_set_option($this->ldap, LDAP_OPT_PROTOCOL_VERSION, 3);
+
+			if(!@ldap_bind($this->ldap, $this->user."@".$this->domain, $this->password)) {
+				$this->ldap = null;
+				throw new \Exception("Unable to Auth");
+			}
+		}
+	}
+
+	/**
+	 * Sync users and groups to the local database
+	 */
+	public function sync() {
+		$this->connect();
+		$this->updateAllUsers();
+		$this->updateAllGroups();
+	}
+
+	/**
+	 * Return an array of permissions for this adaptor
+	 */
+	public function getPermissions() {
+		return array(
+			"addGroup" => false,
+			"addUser" => false,
+			"modifyGroup" => false,
+			"modifyUser" => false,
+			"modifyGroupAttrs" => false,
+			"modifyUserAttrs" => false,
+			"removeGroup" => false,
+			"removeUser" => false,
+			"changePassword" => false
+		);
+	}
+
+	/**
+	 * Get All Users
+	 *
+	 * Get a List of all User Manager users and their data
+	 *
+	 * @return array
+	 */
+	public function getAllUsers() {
+		return parent::getAllUsers('msad');
+	}
+
+	/**
+	* Get All Users
+	*
+	* Get a List of all User Manager users and their data
+	*
+	* @return array
+	*/
+	public function getAllGroups() {
+		return parent::getAllGroups('msad');
+	}
+
+	/**
+	* Get User Information by Username
+	*
+	* This gets user information by username.
+	* !!This should never be called externally outside of User Manager!!
+	*
+	* @param string $id The ID of the user from User Manager
+	* @return array
+	*/
+	public function deleteUserByID($id) {
+		return array("status" => false, "type" => "danger", "message" => _("LDAP is in Read Only Mode. Deletion denied"));
+	}
+
+	/**
+	* Delete a Group by it's ID
+	* @param int $gid The group ID
+	*/
+	public function deleteGroupByGID($gid) {
+		return array("status" => false, "type" => "danger", "message" => _("LDAP is in Read Only Mode. Deletion denied"));
+	}
+
+	/**
+	* Add a user to User Manager
+	*
+	* This adds a new user to user manager
+	*
+	* @param string $username The username
+	* @param string $password The user Password
+	* @param string $default The default user extension, there is an integrity constraint here so there can't be duplicates
+	* @param string $description a short description of this account
+	* @param array $extraData A hash of extra data to provide about this account (work, email, telephone, etc)
+	* @param bool $encrypt Whether to encrypt the password or not. If this is false the system will still assume its hashed as sha1, so this is only useful if importing accounts with previous sha1 passwords
+	* @return array
+	*/
+	public function addUser($username, $password, $default='none', $description=null, $extraData=array(), $encrypt = true) {
+		return array("status" => false, "type" => "danger", "message" => _("LDAP is in Read Only Mode. Addition denied"));
+	}
+
+	/**
+	 * Add a group to User Manager
+	 *
+	 * This adds a new group to User Manager
+	 *
+	 * @param string $groupname   The group Name
+	 * @param string $description The group description
+	 * @param array  $users       users to add to said group (by ID)
+	 */
+	public function addGroup($groupname, $description=null, $users=array()) {
+		return array("status" => false, "type" => "danger", "message" => _("LDAP is in Read Only Mode. Addition denied"));
+	}
+
+	/**
+	 * Update a User in User Manager
+	 *
+	 * This Updates a User in User Manager
+	 *
+	 * @param string $username The username
+	 * @param string $password The user Password
+	 * @param string $default The default user extension, there is an integrity constraint here so there can't be duplicates
+	 * @param string $description a short description of this account
+	 * @param array $extraData A hash of extra data to provide about this account (work, email, telephone, etc)
+	 * @param string $password The updated password, if null then password isn't updated
+	 * @return array
+	 */
+	public function updateUser($uid, $prevUsername, $username, $default='none', $description=null, $extraData=array(), $password=null) {
+		$sql = "UPDATE ".$this->userTable." SET `default_extension` = :default_extension WHERE `id` = :uid";
+		$sth = $this->db->prepare($sql);
+		try {
+			$sth->execute(array(':default_extension' => $default, ':uid' => $uid));
+		} catch (\Exception $e) {
+			return array("status" => false, "type" => "danger", "message" => $e->getMessage());
+		}
+		return array("status" => true, "type" => "success", "message" => _("User updated"), "id" => $uid);
+	}
+
+	/**
+	 * Update Group
+	 * @param string $prevGroupname The group's previous name
+	 * @param string $groupname     The Groupname
+	 * @param string $description   The group description
+	 * @param array  $users         Array of users in this Group
+	 */
+	public function updateGroup($gid, $prevGroupname, $groupname, $description=null, $users=array()) {
+		return array("status" => true, "type" => "success", "message" => _("Group updated"), "id" => $gid);
+	}
+
+	/**
+	 * Check Credentials against username with a passworded sha
+	 * @param {string} $username      The username
+	 * @param {string} $password_sha1 The sha
+	 */
+	public function checkCredentials($username, $password) {
+		$this->connect();
+		$ldap = ldap_connect($this->host,$this->port);
+		if($ldap === false) {
+			return false;
+		}
+		ldap_set_option($ldap, LDAP_OPT_PROTOCOL_VERSION, 3);
+
+		return ldap_bind($ldap, $username, $password);
+	}
+
+	/**
+	 * Update All Groups
+	 * Runs through the directory to update all settings (users and naming)
+	 */
+	private function updateAllGroups() {
+		if(!empty($this->gcache)) {
+			return true;
+		}
+		$this->connect();
+		$sr = ldap_search($this->ldap, $this->dn, "(objectCategory=Group)");
+		$groups = ldap_get_entries($this->ldap, $sr);
+		unset($groups['count']);
+		foreach($groups as $group) {
+			//Now get the users for this group
+			$members = array();
+			$gs = ldap_search($this->ldap, $this->dn, "(&(objectCategory=Person)(sAMAccountName=*)(memberof=".$group['distinguishedname'][0]."))");
+			$users = ldap_get_entries($this->ldap, $gs);
+			unset($users['count']);
+			foreach($users as $user) {
+				$usid = $this->binToStrSid($user['objectsid'][0]);
+				$u = $this->getUserByAuthID($usid);
+				$members[] = $u['id'];
+			}
+			$sid = $this->binToStrSid($group['objectsid'][0]);
+			$this->gcache[$sid] = $group;
+			$um = $this->linkGroup($group['cn'][0], 'msad', $sid);
+			if($um['status']) {
+				$this->updateGroupData($um['id'], array(
+					"description" => !empty($group['description'][0]) ? $group['description'][0] : '',
+					"users" => $members
+				));
+			}
+		}
+	}
+
+	/**
+	 * Update Single User
+	 * @param array $user The user data from usermanager
+	 */
+	private function updateSingleGroup($user) {
+		if(!empty($this->ucache[$user['authid']])) {
+			return true;
+		}
+		$this->connect();
+		$sr = ldap_search($this->ldap, $this->dn, "(&(objectCategory=Group)(objectSID=".$user['authid']."))");
+		$group = ldap_get_entries($this->ldap, $sr);
+		if(empty($group[0])) {
+			return false;
+		}
+		$sid = $this->binToStrSid($group[0]['objectsid'][0]);
+		$this->gcache[$sid] = $group[0];
+		//Now get the users for this group
+		$members = array();
+		$gs = ldap_search($this->ldap, $this->dn, "(&(objectCategory=Person)(sAMAccountName=*)(memberof=".$group[0]['distinguishedname'][0]."))");
+		$users = ldap_get_entries($this->ldap, $gs);
+		unset($users['count']);
+		foreach($users as $user) {
+			$usid = $this->binToStrSid($user['objectsid'][0]);
+			$u = $this->getUserByAuthID($usid);
+			$members[] = $u['id'];
+		}
+		$sid = $this->binToStrSid($group[0]['objectsid'][0]);
+		$this->gcache[$sid] = $group[0];
+		$um = $this->linkGroup($group[0]['cn'][0], 'msad', $sid);
+		if($um['status']) {
+			$this->updateGroupData($um['id'], array(
+				"description" => !empty($group['description'][0]) ? $group['description'][0] : '',
+				"users" => $members
+			));
+		}
+		return true;
+	}
+
+	/**
+	 * Update All Users
+	 */
+	private function updateAllUsers() {
+		if(!empty($this->ucache)) {
+			return true;
+		}
+		$this->connect();
+		$sr = ldap_search($this->ldap, $this->dn, "(&(objectCategory=Person)(sAMAccountName=*))");
+		$users = ldap_get_entries($this->ldap, $sr);
+		unset($users['count']);
+		foreach($users as $user) {
+			$sid = $this->binToStrSid($user['objectsid'][0]);
+			$this->ucache[$sid] = $user;
+			$um = $this->linkUser($user['cn'][0], 'msad', $sid);
+			if($um['status']) {
+				$this->updateUserData($um['id'], array(
+					"description" => !empty($user['description'][0]) ? $user['description'][0] : '',
+					"primary_group" => !empty($user['primarygroupid'][0]) ? $user['primarygroupid'][0] : '',
+					"fname" => !empty($user['givenname'][0]) ? $user['givenname'][0] : '',
+					"lname" => !empty($user['sn'][0]) ? $user['sn'][0] : '',
+					"displayname" => !empty($user['displayname'][0]) ?$user['displayname'][0] : '',
+					"department" => !empty($user['department'][0]) ? $user['department'][0] : '',
+					"email" => !empty($user['mail'][0]) ? $user['mail'][0] : '',
+					"cell" => !empty($user['mobile'][0]) ? $user['mobile'][0] : '',
+					"work" => !empty($user['telephonenumber'][0]) ? $user['telephonenumber'][0] : '',
+				));
+			}
+		}
+	}
+
+	/**
+	 * Update Single User
+	 * @param array $user The user data from usermanager
+	 */
+	private function updateSingleUser($user) {
+		if(!empty($this->ucache[$user['authid']])) {
+			return true;
+		}
+		$this->connect();
+		$sr = ldap_search($this->ldap, $this->dn, "(&(objectCategory=Person)(sAMAccountName=*)(objectSID=".$user['authid']."))");
+		$user = ldap_get_entries($this->ldap, $sr);
+		if(empty($user[0])) {
+			return false;
+		}
+		$sid = $this->binToStrSid($user[0]['objectsid'][0]);
+		$this->ucache[$sid] = $user[0];
+		$um = $this->linkUser($user[0]['cn'][0], 'msad', $this->binToStrSid($user[0]['objectsid'][0]));
+		if($um['status']) {
+			$this->updateUserData($um['id'], array(
+				"description" => !empty($user[0]['description'][0]) ? $user[0]['description'][0] : '',
+				"primary_group" => $user[0]['primarygroupid'][0],
+				"fname" => $user[0]['givenname'][0],
+				"lname" => $user[0]['sn'][0],
+				"displayname" => $user[0]['displayname'][0],
+				"department" => !empty($user[0]['department'][0]) ? $user[0]['department'][0] : '',
+				"email" => !empty($user[0]['mail'][0]) ? $user[0]['mail'][0] : '',
+				"cell" => !empty($user[0]['mobile'][0]) ? $user[0]['mobile'][0] : '',
+				"work" => !empty($user[0]['telephonenumber'][0]) ? $user[0]['telephonenumber'][0] : '',
+			));
+		}
+		return true;
+	}
+
+	/**
+	 * Turns a binary SID into a String
+	 * @param  string $binsid The binary string
+	 */
+	public function binToStrSid($binsid) {
+		$hex_sid = bin2hex($binsid);
+		$rev = hexdec(substr($hex_sid, 0, 2));
+		$subcount = hexdec(substr($hex_sid, 2, 2));
+		$auth = hexdec(substr($hex_sid, 4, 12));
+		$result    = "$rev-$auth";
+
+		for ($x=0;$x < $subcount; $x++) {
+			$subauth[$x] =
+			hexdec($this->littleEndian(substr($hex_sid, 16 + ($x * 8), 8)));
+			$result .= "-" . $subauth[$x];
+		}
+
+		// Cheat by tacking on the S-
+		return 'S-' . $result;
+	}
+
+	/**
+	 * Converts a little-endian hex-number to one, that 'hexdec' can convert
+	 * @param  string $hex hex string
+	 */
+	public function littleEndian($hex) {
+		$result = "";
+
+		for ($x = strlen($hex) - 2; $x >= 0; $x = $x - 2) {
+			$result .= substr($hex, $x, 2);
+		}
+		return $result;
+	}
+}
---
+++ views/msad.php
@@ -0,0 +1,161 @@
+<div class="element-container">
+	<div class="row">
+		<div class="col-md-12">
+			<div class="row">
+				<div class="form-group">
+					<div class="col-md-3">
+						<label class="control-label" for="msad-host"><?php echo _("Host")?></label>
+						<i class="fa fa-question-circle fpbx-help-icon" data-for="msad-host"></i>
+					</div>
+					<div class="col-md-9">
+						<input id="msad-host" name="msad-host" type="text" class="form-control" value="<?php echo isset($config['host']) ? $config['host'] : ''?>">
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+			<span id="msad-host-help" class="help-block fpbx-help-block"><?php echo _("The active directory host")?></span>
+		</div>
+	</div>
+</div>
+<div class="element-container">
+	<div class="row">
+		<div class="col-md-12">
+			<div class="row">
+				<div class="form-group">
+					<div class="col-md-3">
+						<label class="control-label" for="msad-post"><?php echo _("Port")?></label>
+						<i class="fa fa-question-circle fpbx-help-icon" data-for="msad-port"></i>
+					</div>
+					<div class="col-md-9">
+						<input id="msad-port" name="msad-port" type="text" class="form-control" placeholder="389" value="<?php echo isset($config['port']) ? $config['port'] : ''?>">
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+			<span id="msad-port-help" class="help-block fpbx-help-block"><?php echo _("The active directory port")?></span>
+		</div>
+	</div>
+</div>
+<div class="element-container">
+	<div class="row">
+		<div class="col-md-12">
+			<div class="row">
+				<div class="form-group">
+					<div class="col-md-3">
+						<label class="control-label" for="msad-host"><?php echo _("Username")?></label>
+						<i class="fa fa-question-circle fpbx-help-icon" data-for="msad-username"></i>
+					</div>
+					<div class="col-md-9">
+						<input id="msad-username" name="msad-username" type="text" class="form-control" value="<?php echo isset($config['username']) ? $config['username'] : ''?>">
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+			<span id="msad-username-help" class="help-block fpbx-help-block"><?php echo _("The active directory username")?></span>
+		</div>
+	</div>
+</div>
+<div class="element-container">
+	<div class="row">
+		<div class="col-md-12">
+			<div class="row">
+				<div class="form-group">
+					<div class="col-md-3">
+						<label class="control-label" for="msad-password"><?php echo _("Password")?></label>
+						<i class="fa fa-question-circle fpbx-help-icon" data-for="msad-password"></i>
+					</div>
+					<div class="col-md-9">
+						<input id="msad-password" name="msad-password" type="text" class="form-control" value="<?php echo isset($config['password']) ? $config['password'] : ''?>">
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+			<span id="msad-password-help" class="help-block fpbx-help-block"><?php echo _("The active directory password")?></span>
+		</div>
+	</div>
+</div>
+<div class="element-container">
+	<div class="row">
+		<div class="col-md-12">
+			<div class="row">
+				<div class="form-group">
+					<div class="col-md-3">
+						<label class="control-label" for="msad-domain"><?php echo _("Domain")?></label>
+						<i class="fa fa-question-circle fpbx-help-icon" data-for="msad-domain"></i>
+					</div>
+					<div class="col-md-9">
+						<input id="msad-domain" name="msad-domain" type="text" class="form-control" value="<?php echo isset($config['domain']) ? $config['domain'] : ''?>">
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+			<span id="msad-domain-help" class="help-block fpbx-help-block"><?php echo _("The active directory domain")?></span>
+		</div>
+	</div>
+</div>
+<div class="element-container">
+	<div class="row">
+		<div class="col-md-12">
+			<div class="row">
+				<div class="form-group">
+					<div class="col-md-3">
+						<label class="control-label" for="msad-dn"><?php echo _("Base DN")?></label>
+						<i class="fa fa-question-circle fpbx-help-icon" data-for="msad-dn"></i>
+					</div>
+					<div class="col-md-9">
+						<input id="msad-dn" name="msad-dn" type="text" class="form-control" value="<?php echo isset($config['dn']) ? $config['dn'] : ''?>">
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+			<span id="msad-dn-help" class="help-block fpbx-help-block"><?php echo _("The base DN")?></span>
+		</div>
+	</div>
+</div>
+<div class="element-container">
+	<div class="row">
+		<div class="col-md-12">
+			<div class="row">
+				<div class="form-group">
+					<div class="col-md-3">
+						<label class="control-label" for="msad-status"><?php echo _("Status")?></label>
+						<i class="fa fa-question-circle fpbx-help-icon" data-for="msad-status"></i>
+					</div>
+					<div class="col-md-9">
+						<div id="msad-status" class="bg-<?php echo $status['type']?>"><?php echo $status['message']?></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+			<span id="msad-status-help" class="help-block fpbx-help-block"><?php echo _("The connection status of the Active Directory Server")?></span>
+		</div>
+	</div>
+</div>
+<style>
+	#msad-status {
+		padding: 5px;
+		border-radius: 5px;
+		margin-top: 5px;
+	}
+</style>
