670fd560248e2305bba9dd2c4eff2652c6a89b31
Andrew Nagy <andrew.nagy@the159.com>
2016-09-15 19:59:27 -0700

    [Module Tag script: userman 13.0.73.1]

--- Userman.class.php
+++ Userman.class.php
@@ -1026,7 +1026,7 @@ class Userman extends \FreePBX_Helpers implements \BMO {
 	 */
 	private function switchAuth($auth = 'freepbx') {
 		if(!in_array($auth,$this->getAuths())) {
-			throw new \Exception("Unallowed Auth Engine");
+			throw new \Exception("Unallowed Auth Engine [".$auth."]");
 		}
 
 		if(!empty($auth)) {
@@ -1057,8 +1057,8 @@ class Userman extends \FreePBX_Helpers implements \BMO {
 			$name = basename($auth, ".php");
 			if(!class_exists('FreePBX\modules\Userman\Auth\\'.$name)) {
 				include(__DIR__."/functions.inc/auth/modules/".$name.".php");
-				$this->auths[] = $name;
 			}
+			$this->auths[] = $name;
 		}
 		return $this->auths;
 	}
