f45b8d047fa863f6439080445a27367759fd36bc
James Finstrom <jfinstrom@sangoma.com>
2018-07-18 23:24:14 +0000

    Restore cleanup

--- Restore.php
+++ Restore.php
@@ -3,8 +3,11 @@ namespace FreePBX\modules\Userman;
 use FreePBX\modules\Backup as Base;
 class Restore Extends Base\RestoreBase{
   public function runRestore($jobid){
-    $configs = $this->getConfigs();
+    $configs = reset($this->getConfigs());
     $userman = $this->FreePBX->Userman;
+    $configs['directories'] = is_array($configs['directories'])? $configs['directories']:[];
+    $configs['usermanusers'] = is_array($configs['usermanusers'])? $configs['usermanusers']:[];
+    $configs['usermangroups'] = is_array($configs['usermangroups'])? $configs['usermangroups']:[];
     foreach ($configs['directories'] as $dir) {
         if($userman->getDirectoryByID($dir['id']) !== false){
             $userman->updateDirectory($dir['id'], $dir['name'], $dir['active'], $dir['config']);
