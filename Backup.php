e36a75d005c9463a710ed6f5302bcf786401e374
Andrew Nagy <andrew.nagy@the159.com>
2019-02-07 19:48:20 -0500

    Fix issues in backup

--- Backup.php
+++ Backup.php
@@ -2,16 +2,16 @@
 namespace FreePBX\modules\Userman;
 use FreePBX\modules\Backup as Base;
 class Backup Extends Base\BackupBase{
-  public function runBackup($id,$transaction){
-    $userman = $this->FreePBX->Userman();
-    $configs = [
-        'usermanusers' => $userman->bulkhandlerExport('usermanusers'),
-        'usermangroups' => $userman->bulkhandlerExport('usermanugroups'),
-        'directories' => $userman->getAllDirectories(),
-        'defaultdirectory' => $userman->getDefaultDirectory()
-    ];
-    $this->addDirectories($dirs);
-    $this->addDependency('');
-    $this->addConfigs($configs);
-  }
+	public function runBackup($id,$transaction){
+		$userman = $this->FreePBX->Userman();
+		$configs = [
+				'users' => $this->FreePBX->Database->query("SELECT * FROM userman_users")->fetchAll(\PDO::FETCH_ASSOC),
+				'users_settings' => $this->FreePBX->Database->query("SELECT * FROM userman_users_settings")->fetchAll(\PDO::FETCH_ASSOC),
+				'groups' => $this->FreePBX->Database->query("SELECT * FROM userman_groups")->fetchAll(\PDO::FETCH_ASSOC),
+				'groups_settings' => $this->FreePBX->Database->query("SELECT * FROM userman_groups_settings")->fetchAll(\PDO::FETCH_ASSOC),
+				'directories' => $this->FreePBX->Database->query("SELECT * FROM userman_directories")->fetchAll(\PDO::FETCH_ASSOC),
+				'kvstore' => $this->FreePBX->Userman->getAll(null)
+		];
+		$this->addConfigs($configs);
+	}
 }
--- Restore.php
+++ Restore.php
@@ -2,45 +2,66 @@
 namespace FreePBX\modules\Userman;
 use FreePBX\modules\Backup as Base;
 class Restore Extends Base\RestoreBase{
-  public function runRestore($jobid){
-    $configs = reset($this->getConfigs());
-    $userman = $this->FreePBX->Userman;
-    $configs['directories'] = is_array($configs['directories'])? $configs['directories']:[];
-    $configs['usermanusers'] = is_array($configs['usermanusers'])? $configs['usermanusers']:[];
-    $configs['usermangroups'] = is_array($configs['usermangroups'])? $configs['usermangroups']:[];
-    $this->processData($userman, $configs);
-  }
+	private $userman;
 
-  public function processLegacy($pdo, $data, $tables, $unknownTables, $tmpfiledir){
-      $tables = array_flip($tables+$unknownTables);
-      if(!isset($tables['userman_users'])){
-          return $this;
-      }
-      $bmo = $this->FreePBX->Userman;
-      $bmo->setDatabase($pdo);
-        $configs = [
-            'usermanusers' => $bmo->bulkhandlerExport('usermanusers'),
-            'usermangroups' => $bmo->bulkhandlerExport('usermangroups'),
-            'directories' => $bmo->getAllDirectories(),
-            'defaultdirectory' => $bmo->getDefaultDirectory()
-        ];
-      $bmo->resetDatabase();
-      $this->processData($bmo, $configs);
+	public function runRestore($jobid){
+		$configs = $this->getConfigs();
+		$this->userman = $this->FreePBX->Userman;
 
-      return $this;
-  }
-  public function processData($userman,$configs){
-        foreach ($configs['directories'] as $dir) {
-            if ($userman->getDirectoryByID($dir['id']) !== false) {
-                $userman->updateDirectory($dir['id'], $dir['name'], $dir['active'], $dir['config']);
-                continue;
-            }
-            $userman->addDirectory($dir['driver'], $dir['name'], $dir['active'], $dir['config']);
-        }
-        if ($configs['defaultdirectory']) {
-            $userman->setDefaultDirectory($configs['defaultdirectory']);
-        }
-        $userman->bulkhandlerImport('usermangroups', $configs['usermangroups'], true);
-        $userman->bulkhandlerImport('usermanusers', $configs['usermanusers'], true);
-    }
+		$this->processData($configs);
+	}
+
+	public function processLegacy($pdo, $data, $tables, $unknownTables, $tmpfiledir){
+			$tables = array_flip($tables+$unknownTables);
+			if(!isset($tables['userman_users'])){
+					return $this;
+			}
+			$this->userman = $this->FreePBX->Userman;
+
+			$configs = [
+				'users' => $pdo->query("SELECT * FROM userman_users")->fetchAll(\PDO::FETCH_ASSOC),
+				'users_settings' => $pdo->query("SELECT * FROM userman_users_settings")->fetchAll(\PDO::FETCH_ASSOC),
+				'groups' => $pdo->query("SELECT * FROM userman_groups")->fetchAll(\PDO::FETCH_ASSOC),
+				'groups_settings' => $pdo->query("SELECT * FROM userman_groups_settings")->fetchAll(\PDO::FETCH_ASSOC),
+				'directories' => $pdo->query("SELECT * FROM userman_directories")->fetchAll(\PDO::FETCH_ASSOC),
+				'kvstore' => []
+			];
+
+			$this->processData($configs);
+
+			return $this;
+	}
+
+	private function processData($configs){
+		foreach($configs['kvstore'] as $id => $entries) {
+			foreach($entries as $key => $value) {
+				$this->userman->setConfig($key, $value, $id);
+			}
+		}
+
+		$sth = $this->FreePBX->Database->prepare("REPLACE INTO userman_directories (`id`, `name`, `driver`, `active`, `order`, `default`, `locked`) VALUES (:id, :name, :driver, :active, :order, :default, :locked)");
+		foreach($configs['directories'] as $directory) {
+			$sth->execute($directory);
+		}
+
+		$sth = $this->FreePBX->Database->prepare("REPLACE INTO userman_users (`id`, `auth`, `authid`, `username`, `description`, `password`, `default_extension`, `primary_group`, `permissions`, `fname`, `lname`, `displayname`, `title`, `company`, `department`, `language`, `timezone`, `dateformat`, `timeformat`, `datetimeformat`, `email`, `cell`, `work`, `home`, `fax`) VALUES (:id, :auth, :authid, :username, :description, :password, :default_extension, :primary_group, :permissions, :fname, :lname, :displayname, :title, :company, :department, :language, :timezone, :dateformat, :timeformat, :datetimeformat, :email, :cell, :work, :home, :fax)");
+		foreach($configs['users'] as $user) {
+			$sth->execute($user);
+		}
+
+		$sth = $this->FreePBX->Database->prepare("REPLACE INTO userman_groups (`id`, `auth`, `authid`, `groupname`, `description`, `language`, `timezone`, `dateformat`, `timeformat`, `datetimeformat`, `priority`, `users`, `permissions`, `local`) VALUES (:id, :auth, :authid, :groupname, :description, :language, :timezone, :dateformat, :timeformat, :datetimeformat, :priority, :users, :permissions, :local)");
+		foreach($configs['groups'] as $group) {
+			$sth->execute($group);
+		}
+
+		$sth = $this->FreePBX->Database->prepare("REPLACE INTO userman_users_settings (`uid`, `module`, `key`, `val`, `type`) VALUES (:uid, :module, :key, :val, :type)");
+		foreach($configs['users_settings'] as $user) {
+			$sth->execute($user);
+		}
+
+		$sth = $this->FreePBX->Database->prepare("REPLACE INTO userman_groups_settings (`gid`, `module`, `key`, `val`, `type`) VALUES (:gid, :module, :key, :val, :type)");
+		foreach($configs['groups_settings'] as $group) {
+			$sth->execute($group);
+		}
+	}
 }
