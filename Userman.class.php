a41ec815af1dc71cc5b3986c2e47b6267976f1ad
kapil gupta <kgupta@sangoma.com>
2021-06-23 02:57:10 +0000

    FREEI-3528: remove restapps_rtapi_conferences rows for deleted user

--- Userman.class.php
+++ Userman.class.php
@@ -1338,6 +1338,13 @@ class Userman extends \FreePBX_Helpers implements \BMO {
 		if(!$status['status']) {
 			return $status;
 		}
+		if ($this->FreePBX->Modules->checkStatus('sangomartapi')) {
+			$sql = "DELETE FROM restapps_rtapi_conferences WHERE userman_id = ?";
+			$sth = $this->db->prepare($sql);
+			try {
+				$sth->execute(array($id));
+			} catch(\Exception $e) {}
+		}
 		$this->callHooks('delUser',$status);
 		$this->delUser($id,$status);
 		return $status;
