daf06eb1504e4f5816df7cc8b2969ecdb631993e
Adrian Nöthlich <adrian.noethlich@hadiko.de>
2021-06-28 21:06:29 +0200

    FREEPBX-18626: Fix override for objectClassPerson
    adldap2 matches objects to predefined models with traits via the
    objectClass attribute. When defining a custom schema with a different
    objectClass for user objects the method for providing this objectClass
    has to be overridden. adldap2 uses the method objectClassPerson to
    determine the objectClass for users. objectClassUser is not used by the
    library.
    
    Signed-off-by: Adrian Nöthlich <adrian.noethlich@hadiko.de>

--- functions.inc/auth/modules/msad2/Msad2Schema.class.php
+++ functions.inc/auth/modules/msad2/Msad2Schema.class.php
@@ -9,7 +9,8 @@ class Msad2 extends ActiveDirectory {
 	public function __construct($config) {
 		$this->config = $config;
 	}
-	public function objectClassUser() {
+    // objectClassPerson has to be used as adldap2 determines the objectClass of users with it
+	public function objectClassPerson() {
 		return $this->config['userobjectclass'];
 	}
 	public function accountName() {
