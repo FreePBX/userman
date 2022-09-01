c159fe0d3c45edb561fe7fe21ffeae8d5288f070
Megha BM <meghabm@sangoma.com>
2022-09-01 11:34:25 +0000

    Renamed PBXSecurity Module to PBXMfa

--- Userman.class.php
+++ Userman.class.php
@@ -623,12 +623,6 @@ class Userman extends FreePBX_Helpers implements BMO {
 								'type' => $ret['type']
 							);
 						}
-						if (!is_null($password) && $this->FreePBX->Modules->checkStatus('pbxsecurity')) {
-							$passwordExpiryData['id'] = $ret['id'];
-							$passwordExpiryData['username'] = $username;
-							$passwordExpiryData['email'] = isset($extraData['email']) ? $extraData['email'] : "";
-							$this->FreePBX->Pbxsecurity->passwordManagement->resetPasswordExpiry($passwordExpiryData, 'ucp');
-						}
 					}
 					if(!empty($ret['status'])) {
 						if($request['pbx_login'] != "inherit") {
@@ -2509,8 +2503,8 @@ class Userman extends FreePBX_Helpers implements BMO {
 		if($dir['locked']) {
 			return array("status" => false, "message" => _("Directory is locked. Can not update user"));
 		}
-		if (\FreePBX::Modules()->checkStatus('pbxsecurity')) {
-			$res = $this->FreePBX->Pbxsecurity->checkFieldValidationForUserman($uid, $_POST, $extraData);
+		if (\FreePBX::Modules()->checkStatus('pbxmfa')) {
+			$res = $this->FreePBX->Pbxmfa->checkFieldValidationForUserman($uid, $_POST, $extraData);
 			if (!$res['status']) {
 				return $res;
 			}
