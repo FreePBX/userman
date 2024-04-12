b5f448f0f967b4519da966ca632f51a8a2a64e18
Akarsh MR <akarshmr@sangoma.com>
2024-04-12 09:56:25 +0530

    userman active directory syncing syntax errors fixed

--- Console/Userman.class.php
+++ Console/Userman.class.php
@@ -76,55 +76,59 @@ class Userman extends Command {
 	}
 
 	private function syncDirectory($directory,$output,$force=false) {
-		$userman = FreePBX::create()->Userman;
-		if(!$directory['active']) {
-			$output->writeln("Directory '".$directory['name']."' is not active. Skipping");
-			return;
-		}
-		$dir = $userman->getDirectoryObjectByID($directory['id']);
-		if(method_exists($dir,"sync")) {
-			if(!$force && empty($directory['config']['sync'])) {
-				$output->writeln("Directory '".$directory['name']."' sync is None. Skipping (Unless --force flag is set)");
+		if(isset($directory)) {
+			$userman = FreePBX::create()->Userman;
+			if(!$directory['active']) {
+				$output->writeln("Directory '".$directory['name']."' is not active. Skipping");
 				return;
 			}
-			$timeSince = $userman->getConfig("directory-last-sync-time");
-			$timeSince = !empty($timeSince) ? $timeSince : 0;
-			$timeNow = time();
-			$secondsSince = 0;
-			switch($directory['config']['sync']) {
-				case "*/15 * * * *":
-					$secondsSince = 900;
-				break;
-				case "*/30 * * * *":
-					$secondsSince = 1800;
-				break;
-				case "0 * * * *":
-					$secondsSince = 3600;
-				break;
-				case "0 */6 * * *":
-					$secondsSince = 7200;
-				break;
-				case "0 0 * * *":
-					$secondsSince = 14400;
-				break;
-			}
-			if($force || ($timeNow > ($timeSince + $secondsSince))) {
-				$userman->setConfig("directory-last-sync-time", $timeNow);
-				$output->writeln("Starting Sync on directory '".$directory['name']."'...");
-				$userman->lockDirectory($directory['id']);
-				try {
-					$dir->sync($output);
-				} catch(Exception $e) {
-					$output->writeln("\t<error>".$e->getMessage()."</error>");
+			$dir = $userman->getDirectoryObjectByID($directory['id']);
+			if(method_exists($dir,"sync")) {
+				if(!$force && empty($directory['config']['sync'])) {
+					$output->writeln("Directory '".$directory['name']."' sync is None. Skipping (Unless --force flag is set)");
+					return;
 				}
-				$userman->unlockDirectory($directory['id']);
-				$output->writeln("Finished");
+				$timeSince = $userman->getConfig("directory-last-sync-time");
+				$timeSince = !empty($timeSince) ? $timeSince : 0;
+				$timeNow = time();
+				$secondsSince = 0;
+				switch($directory['config']['sync']) {
+					case "*/15 * * * *":
+						$secondsSince = 900;
+					break;
+					case "*/30 * * * *":
+						$secondsSince = 1800;
+					break;
+					case "0 * * * *":
+						$secondsSince = 3600;
+					break;
+					case "0 */6 * * *":
+						$secondsSince = 7200;
+					break;
+					case "0 0 * * *":
+						$secondsSince = 14400;
+					break;
+				}
+				if($force || ($timeNow > ($timeSince + $secondsSince))) {
+					$userman->setConfig("directory-last-sync-time", $timeNow);
+					$output->writeln("Starting Sync on directory '".$directory['name']."'...");
+					$userman->lockDirectory($directory['id']);
+					try {
+						$dir->sync($output);
+					} catch(Exception $e) {
+						$output->writeln("\t<error>".$e->getMessage()."</error>");
+					}
+					$userman->unlockDirectory($directory['id']);
+					$output->writeln("Finished");
+				} else {
+					$output->writeln("Not syncing directory for another ".(($timeSince + $secondsSince)-$timeNow)." seconds");
+				}
+	
 			} else {
-				$output->writeln("Not syncing directory for another ".(($timeSince + $secondsSince)-$timeNow)." seconds");
+				$output->writeln("Directory '".$directory['name']."' does not support syncing");
 			}
-
 		} else {
-			$output->writeln("Directory '".$directory['name']."' does not support syncing");
+			$output->writeln("Directory not found");
 		}
 	}
 
