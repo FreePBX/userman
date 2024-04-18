588e1830bc732cb77755746dcf0266a9bed362e2
kapil gupta <kgupta@sangoma.com>
2024-04-17 21:56:33 -0400

    fixing default value issue

--- functions.inc/auth/modules/Openldap2.php
+++ functions.inc/auth/modules/Openldap2.php
@@ -560,7 +560,7 @@ class Openldap2 extends Auth {
 				'type' 		=> 'text',
 				'index'		=> false,
 				'required'	=> false,
-				'default'	=> $defaults['realmedpasswd'],
+				'default'	=> $defaults['realmedpasswdattr'],
 				'opts'		=> array(
 					'value' => isset($config['realmedpasswdattr']) ? $config['realmedpasswdattr'] : $defaults['realmedpasswdattr'],
 				),
