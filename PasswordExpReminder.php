1287cabeaa3dc98e7ff5dc1ccd679d3596ee479c
Philip Joseph <jphilip@sangoma.com>
2025-10-30 11:50:00 +0000

    FREEI-2494 Check the user exists in the saml table

--- PasswordExpReminder.php
+++ PasswordExpReminder.php
@@ -279,7 +279,9 @@ class PasswordExpReminder {
             $getUser = $this->Userman->getUserByUsername($username);
             if(!empty($getUser)){
                 $usamlenabled = $this->Userman->getCombinedModuleSettingByID($getUser['id'], 'pbxsaml', 'enablesaml');
-                if ($usamlenabled) {
+                // check saml table has the entry for this user
+                $isSamlUser = \FreePBX::Pbxsaml()->isSamlUser($getUser['id']);
+                if($usamlenabled && \FreePBX::Pbxsaml()->isLicensed() && $isSamlUser){
                     return [
                         'loginfailed' => true,
                         'status'=>false,
